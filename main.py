e1807f7795a1e3a152a5cecafdc97e4809b43079
Armand Soto <armand.e.soto@gmail.com>
2025-07-08 20:40:22 -0400

    Made some boilerplate for main

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+    
+    print("Hello World")
+
+def __init__(self):
+    super().__init__()
+
+class Food:
+    def __init__(self, name, fat, carbs, protein):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
+    
+
+if __name__ == "__main__":
+    main()
+    sys.exit();
